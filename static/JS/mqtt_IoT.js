2021e990017b22abe6c3f9ef3d09938fe4a8b561
kevin-cueva <unimagneill@gmail.com>
2022-11-13 11:19:09 -0500

    Grafica 1 agregada

--- static/JS/mqtt_IoT.js
+++ static/JS/mqtt_IoT.js
@@ -9,16 +9,16 @@ client.onMessageArrived = onMessageArrived;
 
 // connect the client
 client.connect({onSuccess:onConnect});
-varMqtt = 0;
 // called when the client connects
 function onConnect() {
   // Once a connection has been made, make a subscription and send a message.
   console.log("Conecctado a Mosquito");
   client.subscribe("neill003");
-  client.subscribe("neill002");
-  message = new Paho.MQTT.Message("3.3");
-  message.destinationName = "neill003";
-  client.send(message);  
+  client.subscribe("proyecto23");
+  //Es mejor no enviarlo porque el segundo dato no lo lee
+  //message = new Paho.MQTT.Message("3.3");
+  //message.destinationName = "neill003";
+  //client.send(message);  
 }
 
 // called when the client loses its connection
@@ -30,12 +30,17 @@ function onConnectionLost(responseObject) {
 
 // called when a message arrives
 function onMessageArrived(message) {
-  console.log("onMessageArrived:"+message.payloadString);
+    data.number = 0.0;
     if(message.destinationName == "neill003"){
         //document.getElementById('neill003').innerHTML = `${message.payloadString}`;
-        varMqtt = parseFloat(message.payloadString);
-        console.log(varMqtt)
-        globales.voltaje = varMqtt;
-
+        
+        var varMqtt = new Object();
+        varMqtt = JSON.parse(message.payloadString.toString());
+        data.mulVol = varMqtt.mulVol;
+        data.mulAmp = varMqtt.mulAmp;
+        data.func1 = varMqtt.func1;
+       if(data.number == NaN){
+        data.number = v;
+       }
     }
 }
